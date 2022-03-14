606002cac95d9164a3983051984eca068ed5d3c7
garlicgrove <99361383+garlicgrove@users.noreply.github.com>
2022-03-13 21:47:16 -0400

    Update 01_variables.py

--- 01_variables.py
+++ 01_variables.py
@@ -4,19 +4,19 @@
 def main():
     # bool - is a boolean ex: True, False
     b = True
-    print(f"Varible b = {b} and is type {type(b)}")
+    print(f"Variable b = {b} and is type {type(b)}")
     # int - which is an integer number (no decimals) ex: -1, 0, 1, 2, 3 ....
     i = 1
-    print(f"Varible i = {i} and is type {type(i)}")
+    print(f"Variable i = {i} and is type {type(i)}")
     # float - which is a real number (is a decimal number) ex: 1.0, 3.14
     f = 1.0
-    print(f"Varible f = {f} and is type {type(f)}")
+    print(f"Variable f = {f} and is type {type(f)}")
     # str - which is a word (or "string" of characters)
     s = "garlic"
-    print(f"Varible s = {s} and is type {type(s)}")
+    print(f"Variable s = {s} and is type {type(s)}")
     # list - a collection of values
     l = [1, 2.0, "a"]
-    print(f"Varible l = {l} and is type {type(l)}")
+    print(f"Variable l = {l} and is type {type(l)}")
 
     x = 2
     y = 3
