049ff562ffd1276d6e2313d33e30cfffe41d10ba
garlicgrove <99361383+garlicgrove@users.noreply.github.com>
2022-03-20 14:24:12 -0400

    loops

--- 03_loops.py
+++ 03_loops.py
@@ -1,6 +1,7 @@
 def main():
     # range is creating a list of numbers 0 - 9
     # the loop will assign the value to i each iteration of the loop
+    print(f"for loop")
     for i in range(10):
         print(f"i is {i}")
 
@@ -8,6 +9,13 @@ def main():
     print()
     for fruit in fruits:
         print(f"the fruit is {fruit}")
+
+    print()
+    print(f"while loop")
+    j = 0
+    while j < 5:
+        print(f"j is {i}")
+        i += 1
 
 if __name__ == '__main__':
     main()
